71e0f532c9e1c04172b4ed2c5e6632844bbc290c
BlezynUkatu <blessingukatu1@gmail.com>
2022-07-03 01:16:04 -0700

    commit

--- 0x03-python-data_structures/8-main.py
+++ 0x03-python-data_structures/8-main.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-multiple_returns = _import_('8-multiple_returns').multiple_returns
-
-sentence = "At school, I learnt C!"
-length, first = multiple_returns(sentence)
-print("Length: {:d} - First character: {}".format(length, first))
+def multiple_returns(sentence):
+        if sentence == "":
+                    return (0, None)
+                    return (len(sentence), sentence[0])
