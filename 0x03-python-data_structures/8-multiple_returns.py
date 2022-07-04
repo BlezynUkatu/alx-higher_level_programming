2dc263c61bfe58cfdd80a56d7b964130c88f18e7
BlezynUkatu <101553471+BlezynUkatu@users.noreply.github.com>
2022-07-04 10:36:50 +0100

    return multiples

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-        return (len(sentence), sentence[0] if len(sentence) > 0 else None)
+    if sentence == "":
+        return (0, None)
+    return (len(sentence), sentence[0])
